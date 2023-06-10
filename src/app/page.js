7cc4efe1c2c52abba2f764f61bc112ccb87b57f8
JISMON THOMAS <jismonthomas18@gmail.com>
2023-06-09 20:16:39 -0700

    all compo hidden

--- src/app/page.js
+++ src/app/page.js
@@ -6,24 +6,24 @@ import Slider from '@/components/Slider';
 
 export default async function Home() {
     const client = createClient();
-    const document = await client.getSingle("home");
+    // const document = await client.getSingle("home");
 
     // console.log('client', client);
     // console.log('document:', document.data);
 
-    const header = document.data.slices.filter(slice => slice.slice_type === 'header');
-    const slider = document.data.slices.filter(slice => slice.slice_type === 'image_slider');
+    // const header = document.data.slices.filter(slice => slice.slice_type === 'header');
+    // const slider = document.data.slices.filter(slice => slice.slice_type === 'image_slider');
 
 
     return (
         <main>
             <PageHeader
-                headerSlice={header}
+            // headerSlice={header}
             />
             <section className='bg-gradient-to-b from-primary-blue from-20% to-transparent to-20%'>
                 <div className='section-wrapper'>
                     <div className='container'>
-                        <Slider content={slider} />
+                        {/* <Slider content={slider} /> */}
                     </div>
                 </div>
             </section>
