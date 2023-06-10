15f0e1f77354436a18143333deefc61f0a54fbb9
JISMON THOMAS <jismonthomas18@gmail.com>
2023-06-09 23:49:15 -0700

    bug fix

--- src/app/page.js
+++ src/app/page.js
@@ -34,7 +34,7 @@ export default async function Home() {
             <section className='bg-gradient-to-b from-primary-blue from-20% to-transparent to-20%'>
                 <div className='section-wrapper'>
                     <div className='container'>
-                        <Slider content={slider} />
+                        {/* <Slider content={slider} /> */}
                     </div>
                 </div>
             </section>
