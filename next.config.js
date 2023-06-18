57482ad7ebb90e0d7498f4d93f677a27370ab2cb
JISMON THOMAS <jismonthomas18@gmail.com>
2023-06-17 23:56:08 -0700

    updated access token

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        accessToken: 'MC5aSTZlUEJBQUFDSUF2RTVJ.ZA7vv73vv73vv73vv70377-977-9cO-_vQhOGgIrLO-_ve-_vTbvv73vv71077-9Eu-_vWoB77-9HGDvv70',
+        accessToken: 'MC5aSTZxWmhBQUFDTUF2Ri1n.77-9Bu-_ve-_ve-_ve-_vWLvv71t77-977-977-9K--_ve-_vVDvv73vv73vv73vv73vv71y77-9IR3vv73vv73vv73vv70L77-977-9',
     },
 };
 
