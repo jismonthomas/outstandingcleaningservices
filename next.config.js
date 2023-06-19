dffab13089c857699d3160a59d552d21599fa2aa
JISMON THOMAS <jismonthomas18@gmail.com>
2023-06-18 21:46:49 -0700

    removed prismic key

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        accessToken: 'MC5aSS1WYVJBQUFDRUF2YmRX.77-9D3cW77-9CO-_ve-_ve-_ve-_vQrvv71GbB_vv70cfQ_vv73vv70J77-9WO-_ve-_ve-_vUoIK--_vV0',
+        accessToken: process.env.NEXT_PUBLIC_PRISMIC,
         sendgridApiKey: process.env.NEXT_PUBLIC_SENDGRID,
     },
 };
